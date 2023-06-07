3b212bb83110f3827a71b7068c0359d29017ae65
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-08 00:14:03 +0800

    init

---
+++ src/Components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+    <button 
+        @click="handleClick()"
+        class="btn"
+        :style="{background: bg_color}">{{ text }}</button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            text: {
+                type: String,
+                default: 'This is a button'
+            },
+            bg_color: 'green'
+        },
+        methods: {
+            handleClick() {
+                this.$emit('btn-click')
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    .btn {
+        outline: 0;
+        border: 0;
+        // padding: 12px 16px;
+        border-radius: 4px;
+    }
+</style>
