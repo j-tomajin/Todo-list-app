93d45b75863b07715425e8cae06e6379b5b8e9e0
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-08 00:14:13 +0800

    init

---
+++ src/Components/Tasks.vue
@@ -0,0 +1,28 @@
+<template>
+    <div 
+        v-for="task in tasks"
+        :key="task.id"
+        class="tasks"
+    >
+        <Task 
+            @toggle-complete="$emit('toggle-complete', task.id)"
+            :task="task" /> 
+    </div>
+</template>
+
+<script>
+    import Task from './Task.vue';
+
+    export default {
+        name: 'Tasks',
+        props: {
+            tasks: Array,
+        },
+        components: {
+            Task,
+        },
+        emits: ['toggle-complete']
+    }
+</script>
+
+<style lang="scss" scoped></style>
