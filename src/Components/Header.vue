cb103fddd13fdcb0fb1311a7fc72bbb6d147cd7a
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-08 00:14:06 +0800

    init

---
+++ src/Components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header class="header">
+        <h1 class="text-head">TODO</h1>
+        <Button text="" bg_color="black" />
+    </header>
+</template>
+
+<script>
+    import Button from './Button.vue';
+
+    export default {
+        name: 'Header',
+        components: {
+            Button,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .text-head {
+        color: white;
+        letter-spacing: 3px;
+        font-size: 3rem;
+    }
+</style>
