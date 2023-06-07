4a075ad2c2d6ebee843c47fd101785876f33ee8d
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-08 00:14:09 +0800

    init

---
+++ src/Components/Task.vue
@@ -0,0 +1,44 @@
+<template>
+    <div :class="[task.complete ? 'complete' : '', 'task']">
+        <Button 
+            class="btn" 
+            text="" :bg_color="[task.complete ? 'black' : 'transparent']" 
+            @btn-click="$emit('toggle-complete', task.id)"
+        />
+        <h3>{{ task.text }}</h3>
+    </div>
+</template>
+
+<script>
+    import Button from './Button.vue';
+
+    export default {
+        name: 'Task',
+        props: {
+            task: Object,
+        },
+        components: {
+            Button,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .task {
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        gap: 24px;
+    }
+
+    .complete {
+        color: rgb(112, 112, 112);
+        text-decoration: line-through;
+    }
+
+    .btn {
+        padding: 12px;
+        border: 1px solid #444;
+        border-radius: 50px;
+    }
+</style>
